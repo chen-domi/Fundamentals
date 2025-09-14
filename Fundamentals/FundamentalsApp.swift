c6e3cc4554d3af7845c453cf8d43889ee1d92f15
chen-domi <cchendominic@gmail.com>
2025-09-13 21:48:24 -0400

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Dominic Chen on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
